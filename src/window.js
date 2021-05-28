62c81694c3dbfc36560aeba5382accdfd2252369
DiegoIvan <diegoivan.mae@gmail.com>
2021-05-27 19:14:10 -0500

    se ha terminado el programa

--- src/window.js
+++ src/window.js
@@ -31,7 +31,9 @@ var PlanoWindow = GObject.registerClass({
     	'resultsFormula',
     	'resultsMidpoint',
     	'clearButton',
-    	'calculateResults'
+    	'calculateResults',
+    	'revealerWarning',
+    	'buttonClose'
     ]
 }, class PlanoWindow extends Gtk.ApplicationWindow {
     _init(application) {
@@ -44,32 +46,46 @@ var PlanoWindow = GObject.registerClass({
         	this._coordY2
         ]
         
+        const allEntries = [
+        	this._resultsSlope,
+    		this._resultsFormula,
+    		this._resultsMidpoint,
+        ]
+        
 	    // clear the EntryBuffer
         this._clearButton.connect ('clicked', () => {
         	allSpinButtons.forEach (spinButton => {
         		spinButton.set_value (0);
         	});
+        	
+        	allEntries.forEach (entry => {
+        		entry.text = "";
+        	});
         });
-        
         this._calculateResults.connect ('clicked', () => {
-        	let sumX, sumY;
-        	let slope;
-        	
-        	sumX = this._coordX2.value - this._coordX1.value;
-        	sumY = this._coordY2.value - this._coordY1.value;
-        	slope = sumY/sumX;
-        	
-        	if (slope === 1) {
-        		this._resultsSlope.text = slope;
+        	if (this._valuesGreaterThanZero ()) {
+        		let sumX, sumY;
+		    	let slope;
+		    	
+		    	sumX = this._coordX2.value - this._coordX1.value;
+		    	sumY = this._coordY2.value - this._coordY1.value;
+		    	slope = sumY/sumX;
+		    	
+		    	this._resultsSlope.text = `${sumY}/${sumX}`;
+		    	log ("calculos completados");
+		    	
+		    	sumX /=2;
+		    	sumY/=2;
+		    	this._resultsMidpoint.text = `(${sumX}, ${sumY})`;
+		    	this._calculateFormula (slope);	
         	}
         	else {
-        		this._resultsSlope.text = `${sumY}/${sumX}`;
+        		this._revealerWarning.set_reveal_child (true);
         	}
-        	
-        	sumX /=2;
-        	sumY/=2;
-        	this._resultsMidpoint.text = `(${sumX}, ${sumY})`;
-        	this._calculateFormula (slope);
+        });
+        
+        this._buttonClose.connect ('clicked', () => {
+        	this._revealerWarning.set_reveal_child (false);
         });
     }
     
@@ -85,6 +101,27 @@ var PlanoWindow = GObject.registerClass({
     		return;
     	}
     	this._resultsFormula.text = `y = ${truncedSlope}x + ${intercept}`;
+    }
+    
+    _valuesGreaterThanZero () {
+    	let counter = 0;
+    	let coordinatesOne = [
+    		this._coordX1,
+    		this._coordY1
+    	];
+    	let coordinatesTwo = [
+    		this._coordX2,
+    		this._coordY2
+    	];
+    	
+    	coordinatesOne.forEach (coordinates => {
+    		if (coordinates.value === 0) {
+    			counter++;
+    			log (counter);
+    		}
+    	});
+    	if (counter === 2) return false;
+    	return true;
     }
 });
 
